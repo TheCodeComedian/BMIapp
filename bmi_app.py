9e96f1bdaa3e94e5bfd97f812061aba4b4372a1e
thecodecomedian <bagnilabhra500@gmail.com>
2025-09-17 23:49:59 +0530

    bmi_app.py

--- bmi_app.py
+++ bmi_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 st.title("This is the Ai-based BMI calculator. Know your health!")
 name= st.text_input("What is your name?")
-wt= st.number_input("Enter your weight")
+wt= st.number_input("Enter your weight in kg")
 ht= st.slider("Enter your height in cm",122,213)
 st.write(f"Selected: {ht}")
 g= st.selectbox("Choose an option:",["Female","Male"])
@@ -22,5 +22,6 @@ if name and wt>0:
     prompt= f"Act like a nutritionist and tell me whether with the given height of {ht}, weight of {wt}, sex of {g} and BMI of {bmi}, the individual seems healthy or not. Just reply in a couple of sentences."
     response= Model.generate_content(prompt)
     st.markdown(response.text)
+
 
 
