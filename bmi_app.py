fa81b192d558f5506ad8948a16dbce5512adb543
thecodecomedian <bagnilabhra500@gmail.com>
2025-09-17 15:30:33 +0530

    bmi_app.py

--- bmi_app.py
+++ bmi_app.py
@@ -8,17 +8,19 @@ st.write(f"Selected: {ht}")
 g= st.selectbox("Choose an option:",["Female","Male"])
 st.write(f"Selected: {g}")
 
-#calculating BMI
-bmi= round(wt/(ht/100)**2,2)
-st.write(f"Hi {name}! With your height {ht}cm and weight {wt}kg, your BMI is: {bmi}")
-
 #getting Ai response
 import google.generativeai as GenAi
 GenAi.configure(api_key="AIzaSyBwbdz4JyJM7BQKQUH8-0EioH5Y7OzfogY")
 Model = GenAi.GenerativeModel(model_name= "gemini-2.5-flash-lite")
 
 if name and wt>0:
+    #calculating BMI
+    bmi= round(wt/(ht/100)**2,2)
+    st.write(f"Hi {name}! With your height {ht}cm and weight {wt}kg, your BMI is: {bmi}")
+
+
     prompt= f"Act like a nutritionist and tell me whether with the given height of {ht}, weight of {wt}, sex of {g} and BMI of {bmi}, the individual seems healthy or not. Just reply in a couple of sentences."
     response= Model.generate_content(prompt)
     st.markdown(response.text)
+
 
